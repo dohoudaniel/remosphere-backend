92dcf3e602dd0ff6741f6b2710d3c15d50b6f9ae
dohoudaniel <dohoudanielfavour@gmail.com>
2025-11-19 19:36:24 +0100

    feat: set up categories functionality

--- remosphere/urls.py
+++ remosphere/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -45,4 +45,7 @@ urlpatterns = [
 
     # JSON schema view
     re_path(r'^swagger.json$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
+    # The job categories
+    path('api/', include('categories.urls')),
 ]
