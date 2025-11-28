30b95be0c0721ce3a61ecbf9ac95d1b7b64df355
dohoudaniel <dohoudanielfavour@gmail.com>
2025-11-28 06:56:49 +0100

    docs: API Docs for Frontend + debug settings for localhost frontend

--- remosphere/settings.py
+++ remosphere/settings.py
@@ -264,5 +264,6 @@ CSRF_COOKIE_SECURE = JWT_COOKIE_SECURE
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://remosphere.onrender.com"
+    "https://remosphere.onrender.com",
+    "http://localhost:3000",
 ]
